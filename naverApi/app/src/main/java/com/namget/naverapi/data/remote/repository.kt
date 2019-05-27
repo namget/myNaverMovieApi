3adf87c1a04d138661c0f02e0d15c5bd9e97e681
namget <yeboykr@naver.com>
2019-05-27 22:21:19 +0900

    Delete repository.kt

--- naverApi/app/src/main/java/com/namget/naverapi/data/remote/repository.kt
+++
@@ -1,8 +0,0 @@
-package com.namget.naverapi.data.remote
-
-import com.namget.naverapi.data.model.MovieResult
-import io.reactivex.Single
-
-interface Repository {
-    fun getMovieList(query: String, display: Int?): Single<MovieResult>
-}
